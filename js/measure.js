2300453222921eeaefd39342a237c92d9780edda
kuyoonjo <yu.chen@live.ie>
2015-01-23 12:42:49 +0000

    build-in function "time" removed. new profiling algorithm added.

--- js/measure.js
+++ js/measure.js
@@ -2,17 +2,34 @@
  * Created by yuchen on 12/5/14.
  */
 
-function measure(lang, cache, sout, serr, err) {
-    var exec = require('child_process').exec,
-        child;
+function measure(lang, file, succeed, fail, mem) {
+    var spawn = require('child_process').spawn;
+    var result = spawn(lang, [file]);
+    var finished = false;
+    var timer;
+    var time = 0;
+    var usage = require('usage');
+    var pid = result.pid;
+    var options = { keepHistory: true };
 
-    child = exec('/usr/bin/time -lp ' + lang + " " + cache + '> cache/' + lang + '.op',
-        {maxBuffer: 1024*1024},
-        function (error, stdout, stderr) {
-            sout(stdout);
-            serr(stderr);
-            if (error !== null) {
-                err(error);
-            }
+    result.stdout.on('data', function (data) {
+        succeed(data);
+    });
+
+    result.stderr.on('data', function (data) {
+        fail(data);
+    });
+
+    result.on('close', function (code) {
+        finished = true;
+        clearInterval(timer);
+    });
+
+    timer = setInterval(function() {
+        usage.lookup(pid, options, function (err, stat) {
+            mem(stat, finished, time);
+            time = time + 1;
         });
+    }, 1);
+    //usage.clearHistory(pid);
 }
